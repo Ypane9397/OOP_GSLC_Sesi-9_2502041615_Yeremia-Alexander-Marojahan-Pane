d45fbcdac2344eb712c2ad3130b5d0dcf6eaa063
Ypane9397 <121032062+Ypane9397@users.noreply.github.com>
2022-12-19 21:08:12 +0700

    Update Function.java

--- GSLC OOP Sesi 9/src/Function.java
+++ GSLC OOP Sesi 9/src/Function.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Function {
 	Random rand = new Random();
-	public int htg(int harga, int uang) {
+	public int htg(int harga, int uang) {//untuk menujukkan kembalian yang akan di dapat pembeli
 		int kembalian = uang - harga;
 		
 		return kembalian;
 	}
 	
-	 public String Date() {
+	 public String Date() {//untuk tanggal pembelian mobil
 	    GregorianCalendar date = new GregorianCalendar();
 	    String namabulan[] = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
 	    String tgl = date.get(Calendar.DAY_OF_MONTH) + " " + namabulan[date.get(Calendar.MONTH)] + " " + date.get(Calendar.YEAR) ;
@@ -18,7 +18,7 @@ public class Function {
 	    return tgl;
 	 }
 	 
-	 public String tx() {
+	 public String tx() {//untuk kode transaksi pembayaran
 	    String hasilrand = "TX" + rand.nextInt(9) + rand.nextInt(9) + rand.nextInt(9) + 
 	                rand.nextInt(9) + rand.nextInt(8)+1;
 	        
